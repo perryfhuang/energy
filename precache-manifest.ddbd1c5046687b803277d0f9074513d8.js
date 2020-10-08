54cdbd57719db131d951d4b85429ee3bffb49b01
perryfhuang <perryfhuang@gmail.com>
2020-10-08 09:22:07 -0400

    Updates

---
+++ precache-manifest.ddbd1c5046687b803277d0f9074513d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b7b852a3c004a9cbcd2954679290f5",
+    "url": "/momentum-clone/index.html"
+  },
+  {
+    "revision": "0df4a205d9bed5759e6c",
+    "url": "/momentum-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc98a0142a62ea224089",
+    "url": "/momentum-clone/static/js/2.c12428d8.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/momentum-clone/static/js/2.c12428d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df4a205d9bed5759e6c",
+    "url": "/momentum-clone/static/js/main.0edc56b4.chunk.js"
+  },
+  {
+    "revision": "acfc5fde7e142b452e6c",
+    "url": "/momentum-clone/static/js/runtime-main.c3abc0ab.js"
+  }
+]);
